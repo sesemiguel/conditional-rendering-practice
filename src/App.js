8fc1b3645380091acfa8fd82d6bbf05bafc2fb86
Sese <sese.miguel9@gmail.com>
2020-07-13 21:07:40 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      isLoggedIn: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  componentDidMount() {
+
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        isLoggedIn: !prevState.isLoggedIn
+      }
+    })
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.isLoggedIn ? <h1>Logged in</h1> : <h1>Logged out</h1>}
+        <button onClick={this.handleClick}>{this.state.isLoggedIn ? 'Logout' : 'Login'}</button>
+      </>
+    )
+  }
 }
 
 export default App;
